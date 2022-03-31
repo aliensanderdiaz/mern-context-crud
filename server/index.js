d50e806cce0976b5afed344758cc31127e30d021
aliensanderdiaz <aliensanderdiaz@gmail.com>
2022-03-30 19:35:11 -0500

    controladores en posts

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
-import express from 'express'
+import app from './app.js'
 import { PORT } from './config.js'
 import { connectDB } from './db.js'
-import postsRoutes from './routes/posts.routes.js'
 
-const app = express()
 connectDB()
-
-app.use(postsRoutes)
 
 app.listen(PORT)
 console.log('Server in running port', PORT)
